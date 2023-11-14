3d1f3fc1e20bbea0e2f7a4fd606343805c912def
Divyesh Kapoor <divyeshkapoor6@gmail.com>
2023-11-14 10:54:31 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,19 @@
+/*let score=undefined
+console.log(typeof score)
+console.log(typeof(score))
+let valueinNumber=Number(score);
+console.log(typeof valueinNumber)
+console.log(valueinNumber)
+*/
+
+//"33"(string) =>33(number)    string conver to number
+//"33abc"(string)=>NaN(Not a number) and its type is number
+//true=>1;false=>0
+
+let isLoggedIn=1
+let booleanIsLoggedIN=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIN);
+
+//1=>true 0=>false
+//"" =>false
+//"Divyesh"=>true
